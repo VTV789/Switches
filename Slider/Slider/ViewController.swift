89bc5bd430fbf762f028b00354acb74dc516a845
Vinh Thanh Vu <vtv@vinh.com>
2015-10-14 22:58:17 -0400

    Setting UISlider

--- Slider/Slider/ViewController.swift
+++ Slider/Slider/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var colorView: UIView!
+    
+    @IBOutlet weak var redController: UISlider!
+    @IBOutlet weak var greenController: UISlider!
+    @IBOutlet weak var blueController: UISlider!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
