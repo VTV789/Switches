f800edea432492767f834fab485cef02b03c5339
Vinh Thanh Vu <vtv@vinh.com>
2015-10-15 23:48:36 -0400

    Create changeColorCompontent

--- Slider/Slider/ViewController.swift
+++ Slider/Slider/ViewController.swift
@@ -22,11 +22,10 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func changeColorCompontent(sender: AnyObject) {
+        
+        
+       
     }
-
-
 }
 
